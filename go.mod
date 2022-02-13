568833038442b23e3745c32dc8424504d9918a2f
Preston Sheldon <pksiv@hotmail.com>
2022-02-13 18:05:49 +0000

    downgrade to v 1.16 of go

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module your_domain/hello
 
-go 1.17
+go 1.16
--- hello.go
+++ hello.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, GO World! built using Tanzu Buildservice")
+    fmt.Println("Hello, GO World! built using OSS kpack")
 }
 
